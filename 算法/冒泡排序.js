beeb0b022493db07bda7f10b5a1185a6d874cc8e
huangting <huangt2012@163.com>
2021-01-25 15:45:26 +0800

    冒泡排序优化

--- 算法/冒泡排序.js
+++ 算法/冒泡排序.js
@@ -2,10 +2,45 @@
 function bubbleSort(arr) {
     const { length } = arr
     for (let i = 0; i < length - 1; i++) {
+        let swapped = false
         for (let j = 0; j < length - 1 - i; j++) {
-            if (arr[j] > arr[j+1]){
-                [arr[j], arr[j+1]] = [arr[j+1], arr[j]]
+            if (arr[j] > arr[j + 1]) {
+                [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]]
+                swapped = true
             }
         }
+        if (!swapped) {
+            break
+        }
     }
+}
+
+function bubbleSort1(arr) {
+    let { length } = arr
+    let swapped = false
+    do {
+        swapped = false
+        for (let i = 1; i < length; i++) {
+            if (arr[i - 1] > arr[i]) {
+                [arr[i - 1], arr[i]] = [arr[i], arr[i - 1]]
+                swapped = true
+            }
+        }
+        length--
+    } while (swapped);
+}
+
+function bubbleSort2(arr) {
+    let len = arr.length
+    let newLen = 0
+    do {
+        newLen = 0
+        for (let i = 1; i < len; i++) {
+            if (arr[i - 1] > arr[i]) {
+                [arr[i - 1], arr[i]] = [arr[i], arr[i - 1]]
+                newLen = i // 记录最后一次交换位置，在此之后的元素在下一轮中不考虑
+            }
+        }
+        len = newLen
+    } while (len > 0)
 }
--- 算法/插入排序.js
+++ 算法/插入排序.js
@@ -2,11 +2,11 @@ function insertionSort(arr) {
   for (let i = 0; i < arr.length; i++) {
     // 寻找元素arr[i]合适的插入位置
     const curr = arr[i]
-    let prevIndex = i - 1
-    while (prevIndex >= 0 && arr[prevIndex] > curr) {
-      arr[prevIndex + 1] = arr[prevIndex]
+    let prevIndex = i
+    while (prevIndex > 0 && arr[prevIndex - 1] > curr) {
+      arr[prevIndex] = arr[prevIndex - 1]
       prevIndex--
     }
-    arr[prevIndex + 1] = curr
+    arr[prevIndex] = curr
   }
 }
